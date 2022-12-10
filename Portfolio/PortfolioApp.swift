22333ab6349fe849ab2490869456452fac7c789f
valentin-droid <valentin.drelon23@icloud.com>
2022-12-10 12:24:34 +0100

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Valentin Drelon on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
