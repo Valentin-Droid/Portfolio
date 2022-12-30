5d9e4ab36211257579f0100306a0c2fc394a84c8
valentin-droid <valentin.drelon23@icloud.com>
2022-12-30 16:48:55 +0100

    faute d'orthographe

--- Portfolio/Helpers/Description.swift
+++ Portfolio/Helpers/Description.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct Description: View {
     var body: some View {
-        Text("Qui je sui :\nJe m'appelle Valentin Drelon je suis née à Brive-la-gaillard le 23-11-2001.\nMon parcoure : \nJe suis détenteur d'un Baccalauréat Scientifique options science de l'ingénieur depuis 2018 au lycée Léonce Vieljeux à La Rochelle.\nJ'ai obtenue mon Brevet de Technicien supérieur en Système numérique informatique et réseau en 2022 à l'ICSSA à Niort. \nJe suis actuellement en Bachelor conception et développement d'application à l'ENI à Niort.")
+        Text("Qui je sui :\nJe m'appelle Valentin Drelon je suis née à Brive-la-gaillard le 23-11-2001.\nMon parcour : \nJe suis détenteur d'un Baccalauréat Scientifique options science de l'ingénieur depuis 2018 au lycée Léonce Vieljeux à La Rochelle.\nJ'ai obtenu mon Brevet de Technicien supérieur en Système numérique informatique et réseau en 2022 à l'ICSSA à Niort. \nJe suis actuellement en Bachelor conception et développement d'application à l'ENI à Niort.")
             .opacity(0.7)
             .italic()
             .frame(width: 350, alignment: .leading)
